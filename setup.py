f618d9f7d17d9686640387913cb78e0a1b1d2d09
YulinChan <chenyulin@yandex.com>
2021-02-22 11:59:59 +0800

    https://www.sehuatang.net/thread-382173-1-1.html

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 import re
 
 import requests
-from pprint import pprint
 from bs4 import BeautifulSoup
 
 
 def film_info(url):
     hd = {
-        'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0'}
+        'user-agent': 'Mozilla/5.0 (x86_64; rv:85.0) Gecko/20100101 Firefox/85.0'}
     r = requests.get(url, headers=hd)
     soup = BeautifulSoup(r.text, 'lxml')
     soup.prettify()
@@ -65,16 +64,12 @@ def film_info(url):
 
 def add_tracker(url):
     r = requests.get(url)
-    trackers = r.text.splitlines()
-    tracker_list = []
-    for i in trackers:
-        if i:
-            tracker_list.append(i)
+    trackers = r.text
     conf = open('aria2.conf', 'a+')
-    conf.write(f"bt-tracker={tracker_list}")
+    conf.write(f'bt-tracker={trackers}')
     conf.close()
     with open('aria2.conf', 'r') as f:
-    	print(f.read())
+        print(f.read())
     print("tracker添加完毕！")
 
 
@@ -82,5 +77,5 @@ if __name__ == '__main__':
     with open('task.txt', 'r') as f:
         link = f.read()
         film_info(link)
-    tracker_url = "https://ngosang.github.io/trackerslist/trackers_best.txt"
+    tracker_url = "https://trackerslist.com/best_aria2.txt"
     add_tracker(tracker_url)
